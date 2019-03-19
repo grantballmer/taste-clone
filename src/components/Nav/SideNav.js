d511f5729dee46e72787e360a614e17a35fbdd72
Grant <grant.a.ballmer@gmail.com>
2019-03-19 10:37:00 +0000

    updated nav styles

--- src/components/Nav/SideNav.js
+++ src/components/Nav/SideNav.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
 const iconPath = process.env.PUBLIC_URL + '/assets/icons';
 
-const SideNav = ({ load, handleClick }) => {
+const SideNav = ({ load, handleClick, auth }) => {
 
   return (
     <div className={`sideNav ${load ? 'displaySideNav' : ''}`}>
@@ -18,11 +19,50 @@ const SideNav = ({ load, handleClick }) => {
           <p>Log In</p>
         </Link>
         
+        <Link to="/signup" className="sideNav__menu--link" onClick={handleClick}>
+          <i className="fas fa-user-plus"></i>
+          <p>Sign Up</p>
+        </Link>
+        
         <Link to="/search" className="sideNav__menu--link" onClick={handleClick}>
           <i className="fas fa-search"></i>
           <p>Search</p>
         </Link>
         
+        <hr />
+        
+        {auth ? (
+        
+          <React.Fragment>
+        
+            <Link to="/watchlist" className={`sideNav__menu--link signed-in`} onClick={handleClick}>
+              <i className="fas fa-clipboard-check"></i>
+              <p>Watchlist</p>
+            </Link>
+            
+            <Link to="/recommendations" className='sideNav__menu--link signed-in' onClick={handleClick}>
+              <i className="far fa-thumbs-up"></i>
+              <p>Recommendations</p>
+            </Link>
+          
+          </React.Fragment>
+        
+        ) : (
+        
+          <React.Fragment>
+          
+            <a className={`sideNav__menu--link signed-out`}>
+              <i className="fas fa-clipboard-check"></i>
+              <p>Watchlist</p>
+            </a>
+            
+            <a className='sideNav__menu--link signed-out'>
+              <i className="far fa-thumbs-up"></i>
+              <p>Recommendations</p>
+            </a>
+          
+          </React.Fragment>
+        )} 
         <hr />
       
         <Link to="/explore/trending" className="sideNav__menu--link" onClick={handleClick}>
@@ -50,4 +90,10 @@ const SideNav = ({ load, handleClick }) => {
   );
 };
 
-export default SideNav;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth.authenticated
+  };
+};
+
+export default connect(mapStateToProps)(SideNav);
